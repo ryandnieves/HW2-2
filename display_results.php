3407cf0001a354e836c732c0d8b8ecb5fcddcccd
ryandnieves <rdn5@njit.edu>
2017-02-05 22:07:50 -0500

    added the date php embed

--- display_results.php
+++ display_results.php
@@ -67,6 +67,9 @@
 
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
+
+	<span><?php echo 'This calculation was done on '.date("m/d/Y"); ?></span><br>
+
     </main>
 </body>
 </html>
